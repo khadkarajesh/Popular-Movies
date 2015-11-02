cb603cff6039551af7ecc317c1d98279362f50df
Rajesh Khadka <rajeshkhadka@lftechnology.com>
2015-11-02 20:30:04 +0545

    remove api key for submission

--- app/src/main/java/com/example/popularmovies/data/Constants.java
+++ app/src/main/java/com/example/popularmovies/data/Constants.java
@@ -4,10 +4,9 @@ package com.example.popularmovies.data;
 public class Constants {
 
     public static final String MOVIE_BASE_URL = "http://api.themoviedb.org/";
-    public static final String API_KEY = "3d9f6ef05faa3072ee2caf7fb6870964";
+    public static final String API_KEY = "YOUR_API_KEY";
 
     public static final String YOUTUBE_INTENT_BASE_URI = "vnd.youtube://";
-    public static final String MOVIE_FRAGMENT = "movieFragment";
     public static final String MOVIE_OBJECT = "movie_object";
 
 }
